0733c30ea72e203ae08c49907aa6c078b1e48375
myxng <mdmj04@naver.com>
2025-05-11 04:14:29 +0900

    Done (cd, pwd, exit)

--- week2/main.c
+++ week2/main.c
@@ -11,7 +11,7 @@
 void print_prompt();
 void run_pipe();
 void run_logic();
-void run_cd(char* args);
+void run_cd(char** args);
 void run_pwd();
 void is_background(char* args, int i);
 void analysis_prompt(char *line);
@@ -74,31 +74,46 @@ void analysis_prompt(char *line)
         args[++i] = strtok(NULL, " \n");
     if (args[0] == NULL) return;
 
-    int bg = is_background(args, i);
+    //int bg = is_background(args, i);
     if (strcmp(args[0], "cd") == 0) run_cd(args);
     else if (strcmp(args[0], "pwd") == 0) run_pwd();
     else if (strcmp(args[0], "exit") == 0) exit(0);
     else printf("Unsupported command: %s\n", args[0]);
 }
 
-void run_cd(char* args)
+void run_cd(char** args)
 {
-    if (args[1]==NULL) 
+    if (args[1]==NULL)
+    {
         fprintf(stderr, "Error: cd argument doesn't exist.\n");
         return;
-    else if (strcmp(args[1],".")==0)
+    }
+    
+    if (strcmp(args[1],".")==0)
         return;
+    
     else if (strcmp(args[1],"..")==0)
-        chdir("..");
+    {
+        if (chdir("..")!=0) perror("cd");
+        return;
+    }
+
     else if (strcmp(args[1],"/")==0)
-        chdir("/");
+    {
+        if (chdir("/")!=0) perror("cd");
+        return;
+    }
+    
     else if (strcmp(args[1],"~")==0)
-        chdir(getenv("HOME"));        
+    {
+        if (chdir(getenv("HOME"))!=0) perror("cd");
+        return;
+    }
+           
     else
     {
-        chdir(args[1]);
         if (chdir(args[1])!=0)
-            printf("Error: cd failed to change directory to %d.\n", args[1]);
+            printf("Error: cd failed to change directory to %s.\n", args[1]);
     }
     return;
 }
