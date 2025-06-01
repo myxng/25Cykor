9d909bccb167718ba6402f5fbece3eacd1a09054
myxng <mdmj04@naver.com>
2025-06-02 00:12:54 +0900

    use url parameter

--- week3/src/index.php
+++ week3/src/index.php
@@ -1,3 +1,13 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    <body>
+        <h1>Welcome to my posting site!</h1>
+        <ol>
+            <li><a href="index.php?id=Login">Login</a></li>
+            <li><a href="index.php?id=Register">Register</a></li>
+        </o1>
+    </body>
+</html>
