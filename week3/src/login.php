cf5d8693aefe99d437cd40c76f470c0397e3b3bb
myxng <mdmj04@naver.com>
2025-06-05 00:19:58 +0900

    0604

--- week3/src/login.php
+++ week3/src/login.php
@@ -4,6 +4,12 @@
         <title></title>
     <body>
         <h1>Hello, World!</h1>
+        <form action="login.php" method="post">
+            <p><input type="text" name="id" placeholder="ID"></p>
+            <p><input type="text" name="password" placeholder="PW"></p>
+            <p><button type="submit">Login</button></p>
+        </form>
+
         <a href="create.php">Create</a>
         <form action="create_process.php" method="post">
         <a href="read.php">Read</a><br>
