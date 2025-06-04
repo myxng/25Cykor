bc3495a553ce7b2e688a78c7243f1d8a573d647e
myxng <mdmj04@naver.com>
2025-06-04 22:47:50 +0900

    로그인 세팅

--- week3/src/index.php
+++ week3/src/index.php
@@ -2,14 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>Start Display</title>
     </head>
     <body>
         <h1>Welcome to my posting site!</h1>
-        <form action="login.php">
-            <p><input type="text" name="id"></p>
-            <p><input type="text" name="password"></p>
-            <p><input type="submit"></p>
+        <form action="login.php" method="post">
+            <p><input type="text" name="id" placeholder="ID"></p>
+            <p><input type="text" name="password" placeholder="PW"></p>
+            <p><input type="submit">Login</p>
         </form>
     </body>
 </html>
