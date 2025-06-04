7d0c2ca8a5cf3e4cd84d2fbb002517e759d63503
myxng <mdmj04@naver.com>
2025-06-04 22:57:35 +0900

    Done

--- week3/src/index.php
+++ week3/src/index.php
@@ -3,13 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Start Display</title>
+        <style>
+            ul {
+                list-style-type: none;
+                padding: 0;
+            }
+        </style>
     </head>
     <body>
-        <h1>Welcome to my posting site!</h1>
-        <form action="login.php" method="post">
-            <p><input type="text" name="id" placeholder="ID"></p>
-            <p><input type="text" name="password" placeholder="PW"></p>
-            <p><input type="submit">Login</p>
-        </form>
+        <h1>Welcome to my site!</h1>
+        
+        <ul>
+            <li><a href="read.php"> 게시판 확인 </a></li>
+            <li><a href="login.php"> Login </a></li>
+            <li><a href="register.php"> Register </a></li>
+        </ul>
+    
     </body>
 </html>
