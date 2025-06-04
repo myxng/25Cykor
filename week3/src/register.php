d6615ed06d31bae4245e8eb0724dfd271af56d7e
myxng <mdmj04@naver.com>
2025-06-05 00:35:19 +0900

    회원가입 폼

---
+++ week3/src/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Register</title>
+    <body>
+        <h2>Register</h2>
+        <form method="post">
+            <p><input type="text" name="id" placeholder="ID"></p>
+            <p><input type="text" name="password" placeholder="PW"></p>
+            <p><button type="submit">Register</button></p>
+        </form>
+
+        <p><a href="index.php">← 메인으로</a></p>
+    </body>
+</html>
