39e7eecd52bb8e60c6c72f6601358e2e3be9b360
myxng <mdmj04@naver.com>
2025-06-05 00:42:25 +0900

    에러 수정

--- week3/src/db.php
+++ week3/src/db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("db", "user", "userpw", "board");
+$mysqli = new mysqli("db", "exampleuser", "examplepass", "exampledb");
 if ($mysqli->connect_error)
 {
     die("DB 연결 실패: ". $mysqli->connect_error);
