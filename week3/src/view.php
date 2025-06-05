69f1a585f949f67d124ef89e47e9b37d987c0de7
myxng <mdmj04@naver.com>
2025-06-05 12:26:50 +0900

    작성자, 작성시간, 제목&내용 구현, 리스트로 되돌아가는 버튼 생성

--- week3/src/view.php
+++ week3/src/view.php
@@ -7,3 +7,19 @@ $result = $mysqli->query("SELECT * FROM posts WHERE id = $id");
 $row = $result->fetch_assoc();
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>View Post</title>
+    </head>
+    <body>
+        <h2><?php echo $row['title']; ?></h2>
+        <p><strong>Writer:</strong> <?php echo $row['author']; ?></p>
+        <p><strong>Create Time:</strong> <?php echo $row['created_at']; ?></p>
+        <p><strong>Content</strong></p>
+        <p><?php echo nl2br($row['content']); ?></p>
+
+        <p><a href="read.php"><button type="submit">Go to List</button></a></p>
+    </body>
+</html>
